f118b2e26b54e0313aa680b976fac1645b30e265
techno5489 <cranderson0110@gmail.com>
2017-09-09 17:14:46 +1200

    World loading...

---
+++ Assets/worldLoader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class worldLoader : MonoBehaviour {
+
+	public void loadWorld (string worldName) {
+		print("Loading " + worldName);
+		SceneManager.LoadScene(worldName);
+	}
+
+	public void generateWorld (string worldName) {
+		
+	}
+}
