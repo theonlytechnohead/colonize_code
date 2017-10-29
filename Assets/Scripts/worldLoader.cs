3fe1639ead6e59feb6bb810a4ed16b236103b6dc
techno5489 <cranderson0110@gmail.com>
2017-10-30 07:00:16 +1300

    Adjusted menu and fade in

--- Assets/Scripts/worldLoader.cs
+++ Assets/Scripts/worldLoader.cs
@@ -4,30 +4,15 @@ using UnityEngine.UI;
 
 public class worldLoader : MonoBehaviour {
 
-	public GameObject loadFadeRawImage;
-	Color image;
-	public Color targetColor;
-
-	void Start () {
-		image = loadFadeRawImage.GetComponent<RawImage>().color;
-	}
+	public Color startColour;
+	public Color targetColour;
 
 	void Update () {
-		if (loadFadeRawImage != null) {
-			image = Color.Lerp(image, targetColor, Time.deltaTime);
-			loadFadeRawImage.GetComponent<RawImage>().color = image;
-			if (image.a < 0.05f) {
-				Destroy(loadFadeRawImage);
-			}
-		}
+		startColour = Color.Lerp(startColour, targetColour, Time.deltaTime);
+		GetComponent<Camera>().backgroundColor = startColour;
 	}
 
 	public void loadWorld (string worldName) {
-		print("Loading " + worldName);
 		SceneManager.LoadScene(worldName);
-	}
-
-	public void generateWorld (string worldName) {
-		
 	}
 }
