92b07fa9e6d9db4d3261e18fcc59d78feef7a757
Craig Anderson <cranderson0110@gmail.com>
2017-11-12 11:45:31 +1300

    Build panel
    Build panel begun!

---
+++ Assets/buildPanelController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class buildPanelController : MonoBehaviour {
+
+	public bool visible = false;
+	public Transform hiddenPosition;
+	public Transform normalPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (visible) {
+			transform.position = Vector3.Lerp(transform.position, normalPosition.position, 10f * Time.deltaTime);
+		} else {
+			transform.position = Vector3.Lerp(transform.position, hiddenPosition.position, 10f * Time.deltaTime);
+		}
+	}
+
+	public void toggleVisibility () {
+		visible = !visible;
+		ResetColour();
+	}
+
+	void ResetColour () {
+		EventSystem.current.SetSelectedGameObject(null);
+	}
+}
